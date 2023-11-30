27ad6953090b7e96414bfd8aae05a5c21e37cea8
Anikribhu <95336760+Anikribhu@users.noreply.github.com>
2023-11-30 18:39:56 +0530

    kya hal chal

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBw4ZxQx5VyDsfFq_YP94_xkCpJefpwNCw",
+    authDomain: "netflix-clone-52179.firebaseapp.com",
+    projectId: "netflix-clone-52179",
+    storageBucket: "netflix-clone-52179.appspot.com",
+    messagingSenderId: "382883415422",
+    appId: "1:382883415422:web:acf109fa9a063dc0c2df2f",
+    measurementId: "G-MYELBNR9ND"
+  };
