157b1c46a22b478891657ba56bd62b9a8c2e3b17
Anikribhu <95336760+Anikribhu@users.noreply.github.com>
2023-11-30 19:36:53 +0530

    Navbar Created

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className='nav nav_black'>
+      <div className="nav_contents">
+        <img className='nav_logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='' />
+        <img className='nav_avatar' src='https://i.pinimg.com/474x/1b/71/b8/1b71b85dd741ad27bffa5c834a7ed797.jpg' alt='' />
+      </div>
+    </div>
+  )
+}
+
+export default Nav
